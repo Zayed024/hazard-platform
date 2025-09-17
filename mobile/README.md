f199e2b284d086157536d0a8a8d626f926fe5fd7
zayed <amanzayed90@gmail.com>
2025-09-17 17:36:41 +0530

    no mock data anymore

---
+++ mobile/README.md
@@ -0,0 +1,3 @@
+# synapse_citizen_app
+
+A new Flutter project.
---
+++ mobile/android/app/src/main/kotlin/com/example/synapse_citizen_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.synapse_citizen_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
